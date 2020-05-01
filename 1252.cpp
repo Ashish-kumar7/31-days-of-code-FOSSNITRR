cede340e7f5b3b4bdbc48d7552824f3556f7a219
ashish007kumar <ashishkumar357ak@gmail.com>
2020-05-01 23:09:22 +0530

    1252

--- 1252.cpp
+++ 1252.cpp
@@ -1,3 +1,12 @@
+//Q.1252 Leetcode 
+//Given n and m which are the dimensions of a matrix initialized by zeros and given an array indices where indices[i] = [ri, ci]. 
+//For each pair of [ri, ci] you have to increment all cells in row ri and column ci by 1.
+
+//Return the number of cells with odd values in the matrix after applying the increment to all indices.
+
+
+
+
 class Solution {
 public:
     int oddCells(int n, int m, vector<vector<int>>& indices) {
