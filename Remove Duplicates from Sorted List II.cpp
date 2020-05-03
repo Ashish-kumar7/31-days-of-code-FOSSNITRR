10d53298627aad210a313f3da1a294ee657cedfe
ashish007kumar <ashishkumar357ak@gmail.com>
2020-05-03 22:20:21 +0530

    03-05-2020

---
+++ Remove Duplicates from Sorted List II.cpp
@@ -0,0 +1,20 @@
+
+ListNode* Solution::deleteDuplicates(ListNode* head) {
+    
+  if(head==NULL || head->next==NULL){
+      return head;
+  }
+  
+  if(head->val!=head->next->val){
+      head->next=deleteDuplicates(head->next);
+      return head;
+  }
+  ListNode * curr=head;
+  while(curr!=NULL && curr->val==head->val){
+      curr=curr->next;
+  }
+  
+  return (deleteDuplicates(curr));
+  
+  
+}
